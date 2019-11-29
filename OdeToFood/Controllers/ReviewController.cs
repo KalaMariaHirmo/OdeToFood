f7f1c6a8495508aaeb5ede7e80618c6e9f159727
Holger420 <hirmo.holger@gmail.com>
2019-11-29 11:24:25 +0200

    uhhhhhhhh reede

---
+++ OdeToFood/Controllers/ReviewController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class ReviewController : Controller
+    {
+        // GET: Review
+        public ActionResult Index()
+        {
+			var model = from r in _review
+						orderby r.Country
+						select r;
+            return View(model);
+        }
+
+        // GET: Review/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Review/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Review/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Review/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Review/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Review/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Review/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+		static List<RestaurantReview> _review = new List<RestaurantReview>
+		{
+			new RestaurantReview
+			{
+				Id = 1,
+				Name = "Cinamon Club",
+				City = "London",
+				Country = "UK",
+				Rating = 10,
+			}
+		};
+
+    }
+}
